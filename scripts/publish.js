a651ce2bdc5a47791f0b8653dab03a9738618a72
Ray Davidson <ray@sanlo.io>
2022-12-10 20:43:15 -0800

    Add scripts for publish and release management

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,15 +1,38 @@
 const fs = require('fs-extra');
 
-const args = process.argv.slice(2) || [];
-const auth0env = args[0];
+const bumpVersion = () => {
+  const raw_package_json = fs.readFileSync('./package.json');
+  const package_json = JSON.parse(raw_package_json);
+  let version = package_json.version;
+
+  try {
+    const version_split = version.split('.');
+    let [major, minor, patch] = version_split;
+    patch = Number(patch) + 1;
+    const new_version = [major, minor, patch].join('.');
+    version = new_version;
+  } catch (e) {
+    console.log(e);
+    console.log(`Error >> Using local version number: ${version}`);
+  }
+
+  console.log(`Setting new version as: ${version}`);
+  package_json.version = version;
+  fs.writeFileSync('./package.json', JSON.stringify(package_json, null, 2) + "\n");
+  return version;
+}
+
+const version = bumpVersion();
 
 const folders = [
   {
     path: './build/static/js',
     regex: /^(main).[\w]*.js$/,
-    outputFile: `./dist/${auth0env}.js`
+    outputFile: `./dist/latest/${version}.js`
   }
 ];
+
+fs.emptyDirSync('./dist/latest');
 
 folders.forEach(folder => {
   fs.readdirSync(folder.path).forEach(file => {
@@ -21,47 +44,3 @@ folders.forEach(folder => {
     }
   });
 });
-
-
-// const fs = require('fs');
-// const axios = require('axios');
-
-// const args = process.argv.slice(2) || [];
-// const is_prod = args[0] === 'prod';
-
-// const bumpVersion = async () => {
-//   const raw_package_json = fs.readFileSync('./package.json');
-//   const package_json = JSON.parse(raw_package_json);
-
-//   const prefix = is_prod ? "@sanlo" : "@sanlo-dev";
-//   package_json.name = `${prefix}/react-sdk`;
-
-//   let version = package_json.version;
-//   try {
-//     // Try to fetch the published version and base off that
-//     const URL = "https://registry.npmjs.org/" + package_json.name;
-//     const response = await axios.get(URL);
-//     const published_version_info = response.data;
-//     const published_version = published_version_info['dist-tags'].latest;
-//     version = published_version;
-//     console.log(`Success >> Published version found: ${version}`);
-//     const version_split = version.split('.');
-//     let [major, minor, patch] = version_split;
-//     // TO-DO:
-//     // Update one of the specific numbers based on input parameters
-//     // For now just bump patch
-//     patch = Number(patch) + 1;
-//     package_json.version = [major, minor, patch].join('.');
-//   } catch (e) {
-//     console.log(e);
-//     // Otherwise use the local version
-//     console.log(`Error >> Using local version number: ${version}`);
-//   }
-
-//   console.log(`Current package name: ${package_json.name}`);
-//   console.log(`Setting new version as: ${package_json.version}`);
-
-//   fs.writeFileSync('./package.json', JSON.stringify(package_json, null, 2) + "\n");
-// }
-
-// bumpVersion();

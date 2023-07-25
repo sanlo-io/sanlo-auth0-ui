64d14dbf6e4b26c9f9e1ac99dc4998e73e8e9552
Ray Davidson <ray@sanlo.io>
2023-07-24 21:42:32 -0700

    add replay config

--- src/index.js
+++ src/index.js
@@ -64,9 +64,14 @@ if (process.env.REACT_APP_SENTRY_URL) {
       /metrics\.itunes\.apple\.com\.edgesuite\.net\//i,
     ],
     dsn: process.env.REACT_APP_SENTRY_URL,
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
+    // If the entire session is not sampled, use the below sample rate to sample
+    // sessions when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
     integrations: [new BrowserTracing(), new Sentry.Replay()],
     environment: process.env.REACT_APP_ENV,
-    release: `${process.env.REACT_APP_NAME}@${process.env.REACT_APP_VERSION}`,
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
